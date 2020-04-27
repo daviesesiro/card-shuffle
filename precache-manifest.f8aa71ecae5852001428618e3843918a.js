d7b7973044677d72c26fc04376b0c56964156610
Davies Esiro <ojurereoluwad@gmail.com>
2020-04-28 00:03:05 +0100

    Updates

---
+++ precache-manifest.f8aa71ecae5852001428618e3843918a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca5dd4e941357dff0b6e9fa67b7c979",
+    "url": "/monster-relodex-v2/index.html"
+  },
+  {
+    "revision": "1ca42381b84094f85ec1",
+    "url": "/monster-relodex-v2/static/css/main.516298f4.chunk.css"
+  },
+  {
+    "revision": "70ddce83845687322d10",
+    "url": "/monster-relodex-v2/static/js/2.2a683841.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-relodex-v2/static/js/2.2a683841.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ca42381b84094f85ec1",
+    "url": "/monster-relodex-v2/static/js/main.4379ebc9.chunk.js"
+  },
+  {
+    "revision": "b5ac84d42b9109e018b3",
+    "url": "/monster-relodex-v2/static/js/runtime-main.b07535f1.js"
+  }
+]);
